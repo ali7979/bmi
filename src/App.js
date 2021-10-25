ebdd9723765afcc9fd8af52df7cf9ed6c62fa610
Zoheb Alam <zohebzob@gmail.com>
2021-10-25 23:24:55 +0530

    ali

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
+import bmi from "./images/bmi.png"
 import './App.css';
 
+import { useState } from "react";
+
 function App() {
+const [height, setheight] = useState("")
+const [weight, setweight] = useState("")
+const [rsult, setrsult] = useState(0)
+const [ktext, setktext] = useState("")
+var calc=async function(e){
+  
+await setweight(e.target.value);
+
+}
+var calc2=async function(e)
+{
+ 
+  await setheight(e.target.value)
+  }
+
+var res=()=>{
+ 
+  var wt=parseFloat(weight);
+  var ht=parseFloat(height);
+  console.log(wt)
+  console.log(ht)
+var bmii=wt/(ht*ht);
+console.log(bmii);
+var k=Math.floor(bmii);
+setrsult(k);
+if(k<18.5){
+  setktext("Underweight");
+}
+else if(k>18.5 && k<24.9){
+  setktext("Normal weight");
+}
+else if(k>25 && k<29.9){
+  setktext("Overweight");
+}
+else if(k>30 && k<34.9){
+  setktext("Obesity class I");
+}
+else if(k>35 && k<39.9){
+  setktext("Obesity class II");
+}
+else if(k>40){
+  setktext("Obesity class III");
+}
+else{
+  setktext("");
+}
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="container">
+
+<div className="box">
+
+			
+      <h1 className="heading">BMI CalCulator</h1>
+      <h1 className="res" style={{display:"block"}}>Your BMI is {rsult} {ktext} </h1>
+<img src={bmi}/>
+
+<div className="right">
+<div className="two">
+  <h1> Enter Details</h1>
+<input className="inp" placeholder="Enter Weight (kg)" onChange={calc}></input>
+<input className="inp" placeholder="Enter Height (m)" onChange={calc2}></input>
+</div>
+
+<button class="button" onClick={res}>Click Me</button>
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
   );
 }
 
